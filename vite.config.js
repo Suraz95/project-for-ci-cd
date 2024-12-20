fc4866a08c19a1139b409f83fb1e30b3c7a09f36
SHAIK SURAZ <shaik.suraz50@gmail.com>
2024-12-20 16:52:07 +0530

    modified vite-config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    // Specify the output directory if needed, default is 'dist'
+    outDir: "dist", // Ensures Vite outputs to the dist directory
+    rollupOptions: {
+      input: "/index.html", // Ensure that the entry point is correct for single-page apps (SPA)
+    },
+  },
+});
